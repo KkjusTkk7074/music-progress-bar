bd3d4cfe3d670b6b0e72b078ff20de83c4c47f5a
KkjusTkk7074 <88614346+KkjusTkk7074@users.noreply.github.com>
2021-08-10 23:23:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const progress = async(bar,current, total,slider,size=11) {
         const percent = current / total * size;
         const progbar = new Array(size).fill(bar);
-        progbar[Math.round(percent)] = slider';
+        progbar[Math.round(percent)] = slider;
         return {
             bar: progbar.join(''),
             percent
